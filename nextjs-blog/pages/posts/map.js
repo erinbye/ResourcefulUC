a11482bb24f9206020d32235ec9f919602e16023
Erin <erinbye99@gmail.com>
2020-10-03 14:23:38 -0400

    follow next.js tutorial with changes

---
+++ nextjs-blog/pages/posts/map.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function Map() {
+  return (
+    <Layout>
+      <Head>
+        <title>Map</title>
+      </Head>
+      <h1>Map</h1>
+    </Layout>
+  );
+}
